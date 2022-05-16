a631c584cc9b10ada1d45517a1c8715283ed82b1
jamerri <jamerri@163.com>
2022-05-16 16:22:09 +0800

    fig3完成

--- 00-SCI-02/Fig3.py
+++ 00-SCI-02/Fig3.py
@@ -14,18 +14,64 @@
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.ticker import MultipleLocator
+import numpy as np
 
-# 定义数据
-x_1 = [1.5, ]
-y_1 = [0.5, ]
-z_1 = [3.5, ]
-x_2 = [1, ]
-y_2 = [1, ]
-z_2 = [4.5, ]
-x_3 = [0.5, ]
-y_3 = [1.5, ]
-z_3 = [5.5, ]
+
+# 定义点数据
+x_1 = [1.5, 2.5, 3.5, 4.5, 5.5]
+y_1 = [0.5, 0.45, 0.4, 0.35, 0.3]
+z_1 = [1, 1.5, 2, 2.5, 3]
+x_2 = [1, 2, 3, 4, 5]
+y_2 = [1, 2, 3, 4, 5]
+z_2 = [3, 2.5, 2, 1.5, 1]
+x_3 = [0.5, 0.45, 0.4, 0.35, 0.3]
+y_3 = [1.5, 2.5, 3.5, 4.5, 5.5]
+z_3 = [5, 4.5, 4, 3.5, 3]
+
+# 定义面数据
+x = np.linspace(0, 8, 8)
+y = np.linspace(0, 8, 8)
+X, Y = np.meshgrid(x, y)
+
+# 定义相交线
+xx_1 = [1.5, 3.5]
+yy_1 = [0.5, 0.4]
+zz_1 = [1, 1]
+xx_2 = [1, 2]
+yy_2 = [1, 2]
+zz_2 = [3, 3]
+xx_3 = [0.5, 0.4]
+yy_3 = [1.5, 3.5]
+zz_3 = [5, 5]
+
+# 定义垂直线
+xxx_1 = [3.5, 3.5]
+yyy_1 = [0.4, 0.4]
+zzz_1 = [1, 2]
+xxx_2 = [2, 2]
+yyy_2 = [2, 2]
+zzz_2 = [3, 2.5]
+xxx_3 = [0.4, 0.4]
+yyy_3 = [3.5, 3.5]
+zzz_3 = [5, 4]
+
+
+# 线条立方体
+def plot_linar_cube(ax, x, y, z, dx, dy, dz, color='black'):
+    # fig = plt.figure()
+    # ax = Axes3D(fig)
+    xx = [x, x, x + dx, x + dx, x]
+    yy = [y, y + dy, y + dy, y, y]
+    kwargs = {'alpha': 1, 'color': color}
+    ax.plot3D(xx, yy, [z] * 5, **kwargs)
+    ax.plot3D(xx, yy, [z + dz] * 5, **kwargs)
+    ax.plot3D([x, x], [y, y], [z, z + dz], **kwargs)
+    ax.plot3D([x, x], [y + dy, y + dy], [z, z + dz], **kwargs)
+    ax.plot3D([x + dx, x + dx], [y + dy, y + dy], [z, z + dz], **kwargs)
+    ax.plot3D([x + dx, x + dx], [y, y], [z, z + dz], **kwargs)
+    # plt.title('Cube')
+    # plt.show()
+
 
 # 导入Times New Roman字体
 plt.rc('font', family='Times New Roman', size=12)
@@ -34,34 +80,59 @@ plt.rc('font', family='Times New Roman', size=12)
 fig = plt.figure()
 ax = Axes3D(fig)
 
-# # 初始化视角
-# ax.view_init(elev=10., azim=20)
-
 # 设置坐标轴标题
-ax.set_xlabel('X (m)', fontsize=12)  # x轴标题
-ax.set_ylabel('Y (m)', fontsize=12)  # y轴标题
-ax.set_zlabel('Z (m)', fontsize=12)  # z轴标题
+ax.set_xlabel('X', fontsize=12)  # x轴标题
+ax.set_ylabel('Y', fontsize=12)  # y轴标题
+ax.set_zlabel('Z', fontsize=12)  # z轴标题
 
 # 设置坐标轴范围
 ax.set_xlim(0, 8)
 ax.set_ylim(0, 8)
 ax.set_zlim(0, 6)
 
+# 隐藏坐标轴刻度线
+ax = plt.gca()
+ax.axes.xaxis.set_ticks([])
+ax.axes.yaxis.set_ticks([])
+ax.axes.zaxis.set_ticks([])
+
+# 导入立方体框线
+plot_linar_cube(ax, 0, 0, 0, 8, 8, 6)
+
+# 绘制折线图，添加数据点，设置点的大小
+ax.plot(x_1, y_1, z_1, '-', c='#f7903d', marker='o', markersize=3.5, linewidth=0.75)
+ax.plot(x_2, y_2, z_2, '-', c='#4d85bd', marker='s', markersize=3.5, linewidth=0.75)
+ax.plot(x_3, y_3, z_3, '-', c='#59a95a', marker='^', markersize=3.5, linewidth=0.75)
+
+# 绘制折线图，添加数据点，设置点的大小
+ax.plot(xx_1, yy_1, zz_1, 'k--', linewidth=1)
+ax.plot(xx_2, yy_2, zz_2, 'k--', linewidth=1)
+ax.plot(xx_3, yy_3, zz_3, 'k--', linewidth=1)
+
+# 绘制折线图，添加数据点，设置点的大小
+ax.plot(xxx_1, yyy_1, zzz_1, 'k--', linewidth=1)
+ax.plot(xxx_2, yyy_2, zzz_2, 'k--', linewidth=1)
+ax.plot(xxx_3, yyy_3, zzz_3, 'k--', linewidth=1)
+
 # # 绘制折线图，添加数据点，设置点的大小
-# figure1 = ax.plot(x_1, y_1, z_1, 'b-', marker='o', markersize=3, linewidth=0.5)
-# figure2 = ax.plot(x_2, y_2, z_2, 'r-', marker='s', markersize=3, linewidth=0.5)
-# figure3 = ax.plot(x_3, y_3, z_3, 'k-', marker='^', markersize=3, linewidth=0.5)
-#
-# # 起点位置图案
-# plt.plot(x_1[0], y_1[0], z_1[0], 'k-', marker='o', markersize=4)
-# plt.plot(x_2[0], y_2[0], z_2[0], 'k-', marker='s', markersize=4)
-# plt.plot(x_3[0], y_3[0], z_3[0], 'k-', marker='^', markersize=4)
+# ax.plot(x_1, y_1, '-', c='#f7903d', marker='o', markersize=3.5, linewidth=0.75)
+# ax.plot(x_2, y_2, '-', c='#4d85bd', marker='s', markersize=3.5, linewidth=0.75)
+# ax.plot(x_3, y_3, '-', c='#59a95a', marker='^', markersize=3.5, linewidth=0.75)
+
+# 绘制平面
+ax.plot_surface(X, Y, Z=X * 0 + 1, color='gray', alpha=0.22)
+ax.plot_surface(X, Y, Z=X * 0 + 3, color='gray', alpha=0.22)
+ax.plot_surface(X, Y, Z=X * 0 + 5, color='gray', alpha=0.22)
 
 # 设置figure_size尺寸
 plt.rcParams['figure.figsize'] = (6.0, 8.0)
 
-# # 保存图片
-# plt.savefig(r'./fig2.tiff', bbox_inches='tight', dpi=300)
+# 调整视角(仰角， 方位角)
+ax.view_init(elev=7, azim=-121)
+
+# 保存图片
+plt.savefig(r'./fig2.tiff', bbox_inches='tight', dpi=300)
+
 plt.show()
 
 
